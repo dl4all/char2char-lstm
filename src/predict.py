3f1aab844306facf5108fb6016b8635d0ff138ff
Claudio Scheer <claudioscheer@protonmail.com>
2020-07-05 16:29:27 -0300

    Add get_sample function

---
+++ src/predict.py
@@ -0,0 +1,16 @@
+def get_sample(model, dataset_loader, device, size, prime_text, top_k=5):
+    model.eval()
+    model.to(device)
+    output_text = [character for character in prime_text]
+    previous_hidden_states = model.init_hidden(1)
+    for character in prime_text:
+        predicted_character, previous_hidden_states = model.predict(
+            character, dataset_loader, device, previous_hidden_states, top_k=top_k
+        )
+    output_text.append(predicted_character)
+    for _ in range(size):
+        predicted_character, previous_hidden_states = model.predict(
+            output_text[-1], dataset_loader, device, previous_hidden_states, top_k=top_k
+        )
+        output_text.append(predicted_character)
+    return "".join(output_text)
